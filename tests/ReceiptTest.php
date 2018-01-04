d1a70fcf2dc16becc2ce07643bf1f97536daaab3
Irfan Ullah <irfanullahhosain@gmail.com>
2018-01-04 16:29:04 +0500

    Added setUp, tearDown method and refactor test

--- tests/ReceiptTest.php
+++ tests/ReceiptTest.php
@@ -12,14 +12,37 @@ use TDD\Receipt;
 class ReceiptTest extends TestCase
 {
 
-	public function testtotal()
+
+	public function setUP()
 	{
 		
-		$receipt = new Receipt();
+
+		$this->receipt = new Receipt();
+
+
+	}
+
+
+
+	public function tearDown()
+	{
+
+
+		unset($this->receipt);
+
+	}
+
+
+	public function testTotal()
+	{
+		
+		$input = [ 0, 2, 5, 8];
+
+		$output = $this->receipt->total($input);
 
 		$this->assertEquals(
 			15,
-		 	$receipt->total( [ 0, 2, 5, 8]),
+		 	$output,
 		 	 'When summing the total should be equal to 15'
 		);
 
