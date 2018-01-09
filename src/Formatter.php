d7b860a5caac7db69eaa447b59e7e9718caa2604
Irfan Ullah <irfanullahhosain@gmail.com>
2018-01-09 21:38:00 +0500

    Added Formatter class with FormatterTest and pasted currencyAmount method into it

---
+++ src/Formatter.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace TDD;
+
+class Formatter
+{
+
+
+	public function currenyAmount($input)
+	{
+		
+
+		return round($input, 2);
+
+
+	}
+
+
+}
---
+++ tests/FormatterTest.php
@@ -0,0 +1,75 @@
+<?php
+namespace TDD\Test;
+
+require dirname(dirname(__FILE__)) . DIRECTORY_SEPARATOR.'vendor'. DIRECTORY_SEPARATOR.'autoload.php';
+
+
+use PHPUnit\Framework\TestCase;
+
+use TDD\Formatter;
+
+class FormatterTest extends TestCase
+{
+
+
+
+	public function setUP()
+	{
+		
+ 
+		$this->formatter = new Formatter();
+
+
+	}
+
+
+
+	public function tearDown()
+	{
+
+
+		unset($this->formatter);
+
+	}
+
+
+
+
+	/**
+	 * @dataProvider provideCurrenyAmount
+	*/
+
+	public function testCurrentyAmount($input, $expected, $msg)
+	{
+		
+
+		$this->assertSame(
+			$expected,
+			$this->formatter->currenyAmount($input),
+			 $msg
+		);
+
+	}
+
+
+	public function provideCurrenyAmount()
+	{
+		
+		return [
+
+			[1, 1.00, '1 should be transformed into 1.00'],
+
+			[1.1, 1.10, '1.1 should be transformed into 1.10'],
+			
+			[1.11, 1.11, '1.11 should stay as 1.11'],
+
+			[1.111, 1.11, '1.111 should be transformed into 1.11'],
+
+
+		];
+
+
+	}
+
+ 
+}
